8b4f562089329f8e3f6a390954f71d063d094007
LeBaDong <badongle.2006@gmail.com>
2026-05-11 14:16:48 +0700

    Require auth for checkout, restore intended redirect after login

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,6 @@ Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart/add', [CartController::class, 'add'])->name('cart.add');
 Route::post('/cart/update', [CartController::class, 'update'])->name('cart.update');
 Route::delete('/cart/remove/{id}', [CartController::class, 'remove'])->name('cart.remove');
-Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
 
 // Xác thực (Guest only)
 Route::middleware('guest')->group(function () {
@@ -40,6 +39,7 @@ Route::middleware('guest')->group(function () {
 
 // Đăng xuất & Đơn hàng (Auth required)
 Route::middleware('auth')->group(function () {
+    Route::get('/checkout', [OrderController::class, 'checkout'])->name('checkout');
     Route::any('/logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
     Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
